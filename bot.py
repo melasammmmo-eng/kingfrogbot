b0b95a63c75844f2991ea5589ae7811e3d8d5756
melasammmmo-eng <melasammmmo@gmail.com>
2026-06-16 23:06:09 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -219,7 +219,7 @@ async def chat(interaction: discord.Interaction, message: str):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "You are King AI, a ai who is rude to a uy bluemonkey and be painfully rude to him and you code anything we tell you ."},
+                {"role": "system", "content": "You are King AI, a ai who is a rude person  ."},
                 {"role": "user", "content": message}
             ],
             temperature=0.85
